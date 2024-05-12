34c73bc621923ad78b9c8b3888c5b7ba1f767516
Mattcrmx <matthias.cremieux@hotmail.fr>
2024-05-12 19:01:06 +0200

    chg: :memo: add docstrings and free forgotten mallocs

--- monitor_fd.c
+++ monitor_fd.c
@@ -17,6 +17,12 @@ int process_exists(int pid);
 #define BUF_SIZE 1024
 char buffer[BUF_SIZE];
 
+/**
+ * @brief Check if the given string represents a number.
+ *
+ * @param nb A pointer to the string to be checked.
+ * @return 0 if the string represents a number, 1 otherwise.
+ */
 int is_number(char *nb)
 {
     for (int i = 0; nb[i] != '\0'; ++i)
@@ -29,6 +35,13 @@ int is_number(char *nb)
     return 0;
 }
 
+/**
+ * @brief Check if the number of arguments matches the required number.
+ *
+ * @param args     The number of arguments provided.
+ * @param required The number of arguments required.
+ * @return 0 if the number of arguments matches the required number, 1 otherwise.
+ */
 int check_args(int args, int required)
 {
     if (args != required)
@@ -42,6 +55,13 @@ int check_args(int args, int required)
     }
 }
 
+/**
+ * @brief Allocate memory safely using malloc.
+ *
+ * @param size The size of memory to allocate.
+ * @return A pointer to the allocated memory.
+ * @warning This function terminates the program with EXIT_FAILURE if memory allocation fails.
+ */
 void *safe_malloc(size_t size)
 {
     void *ptr = malloc(size);
@@ -56,6 +76,12 @@ void *safe_malloc(size_t size)
     }
 }
 
+/**
+ * @brief Trim leading and trailing spaces from the given string.
+ *
+ * @param str A pointer to the string to be trimmed.
+ * @return A pointer to the trimmed string.
+ */
 char *trim(char *str)
 {
     char *end;
@@ -77,6 +103,12 @@ char *trim(char *str)
     return str;
 }
 
+/**
+ * @brief Get the process ID (PID) by process name.
+ *
+ * @param name The name of the process.
+ * @return The PID of the process, or -1 if the process is not found.
+ */
 int get_pid_by_name(char name[])
 {
     DIR *proc;
@@ -142,6 +174,12 @@ int get_pid_by_name(char name[])
     return -1;
 }
 
+/**
+ * @brief Check if a process with the given PID exists.
+ *
+ * @param pid The PID of the process to check.
+ * @return 1 if the process exists, 0 otherwise.
+ */
 int process_exists(int pid)
 {
     char *proc_path = safe_malloc(16);
@@ -152,14 +190,22 @@ int process_exists(int pid)
     if (proc_dir)
     {
         closedir(proc_dir);
+        free(proc_path);
         return 1;
     }
     else
     {
+        free(proc_path);
         return 0;
     }
 }
 
+/**
+ * @brief Count the number of file descriptors associated with a process by process name.
+ *
+ * @param name The name of the process.
+ * @return The number of file descriptors associated with the process.
+ */
 int count_descriptors_by_name(char name[])
 {
     // wrapper around the count_descriptors_by_pid function
@@ -169,6 +215,12 @@ int count_descriptors_by_name(char name[])
     return descriptors;
 }
 
+/**
+ * @brief Count the number of file descriptors associated with a process by PID.
+ *
+ * @param pid The PID of the process.
+ * @return The number of file descriptors associated with the process.
+ */
 int count_descriptors_by_pid(int pid)
 {
     int nb_fd = 0;
@@ -191,8 +243,10 @@ int count_descriptors_by_pid(int pid)
     }
     else
     {
+        free(proc_path);
         return -1;
     }
 
+    free(proc_path);
     return nb_fd;
 }
