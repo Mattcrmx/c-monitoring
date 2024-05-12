8e5db0c167b202c22c671eb4313b2b280d5aa1ba
Mattcrmx <matthias.cremieux@hotmail.fr>
2024-05-12 18:17:02 +0200

    chg: :art: correct formatting

--- monitor_fd.c
+++ monitor_fd.c
@@ -9,122 +9,140 @@ int check_args(int args, int required);
 void *safe_malloc(size_t size);
 char *trim(char *str);
 int get_pid_by_name(char name[]);
-int count_descriptors_by_name (char name[]);
-int count_descriptors_by_pid (int pid);
-int is_number (char *nb);
-
+int count_descriptors_by_name(char name[]);
+int count_descriptors_by_pid(int pid);
+int is_number(char *nb);
 
 #define BUF_SIZE 1024
 char buffer[BUF_SIZE];
 
-int is_number (char *nb) {
-    for (int i = 0; nb[i] != '\0'; ++i) {
-        if (!isdigit(nb[i])) {
+int is_number(char *nb)
+{
+    for (int i = 0; nb[i] != '\0'; ++i)
+    {
+        if (!isdigit(nb[i]))
+        {
             return 1;
         }
     }
     return 0;
 }
 
-int check_args (int args, int required) {
-    if (args != required) {
+int check_args(int args, int required)
+{
+    if (args != required)
+    {
         printf("use: ./monitor_fd process_name\n");
         return 1;
     }
-    else {
+    else
+    {
         return 0;
     }
 }
 
-void *safe_malloc (size_t size) {
+void *safe_malloc(size_t size)
+{
     void *ptr = malloc(size);
-    if(ptr != NULL) {
+    if (ptr != NULL)
+    {
         return ptr;
     }
-    else {
+    else
+    {
         fprintf(stderr, "malloc of size %lu failed\n", size);
         exit(EXIT_FAILURE);
     }
 }
 
-char *trim (char *str) {
+char *trim(char *str)
+{
     char *end;
 
     // Trim leading space
-    while(isspace((unsigned char)*str)) str++;
+    while (isspace((unsigned char)*str))
+        str++;
 
-    if(*str == 0)  
+    if (*str == 0)
         return str;
 
     // Trim trailing space
     end = str + strlen(str) - 1;
-    while(end > str && isspace((unsigned char)*end)) end--;
+    while (end > str && isspace((unsigned char)*end))
+        end--;
 
     end[1] = '\0';
 
     return str;
 }
 
-
-int get_pid_by_name (char name[]) {
+int get_pid_by_name(char name[])
+{
     DIR *proc;
     proc = opendir("/proc");
     char *status_path = safe_malloc(50);
     struct dirent *de;
 
-    while ((de = readdir(proc)) != NULL) {
+    while ((de = readdir(proc)) != NULL)
+    {
 
         // skip files
 
-        if (de->d_type != DT_DIR) {
+        if (de->d_type != DT_DIR)
+        {
             continue;
         }
 
         // skip . and ..
-        if (is_number(de->d_name) == 1) {
+        if (is_number(de->d_name) == 1)
+        {
             continue;
         }
 
         sprintf(status_path, "/proc/%s/status", de->d_name);
         FILE *fp;
-        fp  = fopen(status_path, "r");
+        fp = fopen(status_path, "r");
         long length;
 
-        if (fp != NULL) {
+        if (fp != NULL)
+        {
             // read the file's content
-            while (fgets(buffer, BUF_SIZE, fp) != NULL) {
+            while (fgets(buffer, BUF_SIZE, fp) != NULL)
+            {
                 /* Total character read count */
                 length = strlen(buffer);
                 // Trim new line character from last if exists.
-                buffer[length-1] = buffer[length-1] == '\n' ? '\0' : buffer[length-1];
+                buffer[length - 1] = buffer[length - 1] == '\n' ? '\0' : buffer[length - 1];
 
                 // find the name
                 char *res = strstr(buffer, "Name");
-                if (res) {
+                if (res)
+                {
                     char proc_name[255];
                     strcpy(proc_name, trim(&buffer[5]));
                     // printf("name %s - proc_name %s - pid %d\n", name, proc_name, i);
 
-                    if (strcmp(name, proc_name) == 0) {
+                    if (strcmp(name, proc_name) == 0)
+                    {
                         printf("name : %s - pid: %s\n", proc_name, de->d_name);
                         return atoi(de->d_name);
                     }
                 }
             }
         }
-        else {
+        else
+        {
             continue;
         }
-
     }
 
     free(status_path);
 
     return -1;
-
 }
 
-int count_descriptors_by_name (char name[]) {
+int count_descriptors_by_name(char name[])
+{
     // wrapper around the count_descriptors_by_pid function
     int pid = get_pid_by_name(name);
     int descriptors = count_descriptors_by_pid(pid);
@@ -132,10 +150,12 @@ int count_descriptors_by_name (char name[]) {
     return descriptors;
 }
 
-int count_descriptors_by_pid (int pid) {
+int count_descriptors_by_pid(int pid)
+{
     int nb_fd = 0;
 
-    if (pid == -1) {
+    if (pid == -1)
+    {
         printf("process %d was not found on the system.\n", pid);
         return -1;
     }
@@ -146,17 +166,20 @@ int count_descriptors_by_pid (int pid) {
     DIR *proc_dir;
     proc_dir = opendir(proc_path);
 
-    if (proc_dir) {
+    if (proc_dir)
+    {
         struct dirent *de;
-        while ((de = readdir(proc_dir)) != NULL) {
+        while ((de = readdir(proc_dir)) != NULL)
+        {
             nb_fd++;
         }
 
         closedir(proc_dir);
-    } else {
+    }
+    else
+    {
         return -1;
     }
-
 
     return nb_fd;
 }
--- watch_fd.c
+++ watch_fd.c
@@ -13,16 +13,19 @@ const char *argp_program_version = "fd-watcher 0.1";
 static char doc[] = "File descriptor watcher";
 static char args_doc[] = "[FILENAME]...";
 static struct argp_option options[] = {
-        {"time", 't', "TIME", OPTION_ARG_OPTIONAL, "How much time should the watchdog run.", 0},
-        {"name", 'n', "NAME", OPTION_ARG_OPTIONAL, "The name of the process to monitor.", 0},
-        {"pid",  'p', "PID", OPTION_ARG_OPTIONAL, "The pid of the process to monitor.", 0},
-        {"interval",  'i', "INTERVAL", OPTION_ARG_OPTIONAL, "The interval between watch, in seconds.", 0},
-        {0} // needs to be terminated by a zero
+    {"time", 't', "TIME", OPTION_ARG_OPTIONAL, "How much time should the watchdog run.", 0},
+    {"name", 'n', "NAME", OPTION_ARG_OPTIONAL, "The name of the process to monitor.", 0},
+    {"pid", 'p', "PID", OPTION_ARG_OPTIONAL, "The pid of the process to monitor.", 0},
+    {"interval", 'i', "INTERVAL", OPTION_ARG_OPTIONAL, "The interval between watch, in seconds.", 0},
+    {0} // needs to be terminated by a zero
 };
 
-struct arguments {
-    enum {
-        WATCH_BY_NAME, WATCH_BY_PID
+struct arguments
+{
+    enum
+    {
+        WATCH_BY_NAME,
+        WATCH_BY_PID
     } mode;
     char *name;
     int pid;
@@ -30,7 +33,8 @@ struct arguments {
     int interval;
 };
 
-int safe_convert_to_int(char *pid) {
+int safe_convert_to_int(char *pid)
+{
     // helper function to be sure that the provided pid is well formatted.
     // courtesy of https://stackoverflow.com/questions/8871711/atoi-how-to-identify-the-difference-between-zero-and-error
     long lnum;
@@ -41,69 +45,78 @@ int safe_convert_to_int(char *pid) {
 
     lnum = strtol(pid, &end, 10);
 
-    if (end == pid) {
-        //if no characters were converted these pointers are equal
+    if (end == pid)
+    {
+        // if no characters were converted these pointers are equal
         fprintf(stderr, "ERROR: can't convert string to number\n");
         num = -1;
-
-    } else if ((lnum == LONG_MAX || lnum == LONG_MIN) && errno == ERANGE) {
-        //If sizeof(int) == sizeof(long), we have to explicitly check for overflows
+    }
+    else if ((lnum == LONG_MAX || lnum == LONG_MIN) && errno == ERANGE)
+    {
+        // If sizeof(int) == sizeof(long), we have to explicitly check for overflows
         fprintf(stderr, "ERROR: number out of range for LONG\n");
         num = -1;
-
-    } else if ( (lnum > INT_MAX) || (lnum < INT_MIN) ) {
-        //Because strtol produces a long, check for overflow
+    }
+    else if ((lnum > INT_MAX) || (lnum < INT_MIN))
+    {
+        // Because strtol produces a long, check for overflow
         fprintf(stderr, "ERROR: number out of range for INT\n");
         num = -1;
-
-    } else {
-        num = (int) lnum;
+    }
+    else
+    {
+        num = (int)lnum;
     }
 
     return num;
 }
 
-static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
     struct arguments *arguments = state->input;
-    switch (key) {
-        case 'i':
-            arguments->interval = safe_convert_to_int(arg);
-            break;
-        case 't':
-            arguments->time = safe_convert_to_int(arg);
-            break;
-        case 'n':
-            arguments->mode = WATCH_BY_NAME;
-            arguments->name = arg;
-            break;
-        case 'p':
-            arguments->mode = WATCH_BY_PID;
-            arguments->pid = safe_convert_to_int(arg);
-            break;
-        case ARGP_KEY_ARG:
-            return 0;
-        default:
-            return ARGP_ERR_UNKNOWN;
+    switch (key)
+    {
+    case 'i':
+        arguments->interval = safe_convert_to_int(arg);
+        break;
+    case 't':
+        arguments->time = safe_convert_to_int(arg);
+        break;
+    case 'n':
+        arguments->mode = WATCH_BY_NAME;
+        arguments->name = arg;
+        break;
+    case 'p':
+        arguments->mode = WATCH_BY_PID;
+        arguments->pid = safe_convert_to_int(arg);
+        break;
+    case ARGP_KEY_ARG:
+        return 0;
+    default:
+        return ARGP_ERR_UNKNOWN;
     }
 
     // check for error
-    if (arguments->interval == -1 || arguments->time == -1 || arguments->pid == -1) {
+    if (arguments->interval == -1 || arguments->time == -1 || arguments->pid == -1)
+    {
         return 1;
     }
 
     return 0;
 }
 
-int watch(int pid, int interval, int time_limit) {
+int watch(int pid, int interval, int time_limit)
+{
     // function to watch the descriptors of a certain process
     time_t end;
     time_t start = time(NULL);
     int descriptors;
     end = start + time_limit;
 
-    while (start < end) {
+    while (start < end)
+    {
         descriptors = count_descriptors_by_pid(pid);
-        fprintf(stderr,"process %d: %d\n", pid, descriptors);
+        fprintf(stderr, "process %d: %d\n", pid, descriptors);
         sleep(interval);
         start = time(NULL);
     }
@@ -112,12 +125,13 @@ int watch(int pid, int interval, int time_limit) {
 
 static struct argp argp = {options, parse_opt, args_doc, doc, 0, 0, 0};
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
     // default values for the arguments
     struct arguments arguments;
     arguments.mode = WATCH_BY_NAME;
-    arguments.name = (char *) "";  // forced casting to a pointer to avoid the warning: bad
+    arguments.name = (char *)""; // forced casting to a pointer to avoid the warning: bad
     arguments.pid = -1;
     arguments.time = 60;
     arguments.interval = 1;
@@ -131,17 +145,17 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Bad usage\n");
         return 1;
     }
-    
 
     // get the pid
     int pid;
-    switch (arguments.mode) {
-        case WATCH_BY_NAME:
-            pid = get_pid_by_name(arguments.name);
-            break;
-        case WATCH_BY_PID:
-            pid = arguments.pid;
-            break;
+    switch (arguments.mode)
+    {
+    case WATCH_BY_NAME:
+        pid = get_pid_by_name(arguments.name);
+        break;
+    case WATCH_BY_PID:
+        pid = arguments.pid;
+        break;
     }
 
     // watch
